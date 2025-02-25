49cd2fb30e69f3e6f618dfb77adef6320d4ae887
Aastha Sahu <aasthasahu0271@gmail.com>
2025-02-25 12:13:05 +0530

    Added error view

--- TimeTableGenerator/Controllers/TimeTableController.cs
+++ TimeTableGenerator/Controllers/TimeTableController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using TimeTableGenerator.Models;
 
 namespace TimeTableGenerator.Controllers
@@ -96,6 +97,14 @@ namespace TimeTableGenerator.Controllers
                 Schedule = schedule
             };
         }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
 
     }
 }
